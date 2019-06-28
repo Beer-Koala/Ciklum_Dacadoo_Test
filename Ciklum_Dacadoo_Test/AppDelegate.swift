e7a6096c21680fd9af4068be14f22317808f4d78
first commit <>
2019-06-28 23:36:12 +0300

    Created project Added pods Made main design in main.Storyboard

---
+++ Ciklum_Dacadoo_Test/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Ciklum_Dacadoo_Test
+//
+//  Created by BeerKoala on 6/26/19.
+//  Copyright © 2019 a.kryvchykov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
